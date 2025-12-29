9fd1c7f7dd1198235d7dc952de5b9b2887c96966
Nguyễn Minh Khôi <khoinguyendata.work@gmail.com>
2025-12-30 00:16:14 +0700

    Completed config jarspath (#3)
    * branch:change_prj_code layout changing layout
    
    * branch:completed_config_jarspath add, fix database config, jar path

--- _002_src/data_pipeline/_01_config/database_config.py
+++ _002_src/data_pipeline/_01_config/database_config.py
@@ -1,12 +1,7 @@
-# OJDBC JAR File
-ORL_LIB_PATH = {"driver_class_path": "/opt/spark/jars/ojdbc17.jar"}
-
-# Path đến Redshift JDBC jar
-REDSHIFT_JDBC_JAR_PATH = "/opt/spark/jars/redshift-jdbc42-2.2.1.jar"
-
-# Config kết nối Redshift
+# Config connect to Redshift
 REDSHIFT_JDBC = {
     "url": "jdbc:redshift://my-project-e2e-wg.950242545712.ap-southeast-1.redshift-serverless.amazonaws.com:5439/my-project-e2e-dtb",
+    'tempdir': 's3://data-pipeline-e2e-datalake-98c619f9/redshift-temp/',
     "properties": {
         "user": "admin",
         "password": "Devdata123"
